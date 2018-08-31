6d181b56d8305bbd2e54a24235de31fc3ce385ee
Josh Shih <josh.shih@ARICENT.COM>
2018-08-31 14:56:44 -0700

    variables in templates

--- templates/main.go
+++ templates/main.go
@@ -6,13 +6,28 @@ import (
 	"os"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("*.gohtml"))
+}
+
 func main() {
-	tpl, err := template.ParseGlob("*.gohtml")
-	if err != nil {
-		log.Fatalln("Error parsing files.", err)
-	}
+	//parseGlob allows pattern filtering
 	//tpl.Execute(os.Stdout, nil)
-	tpl.ExecuteTemplate(os.Stdout, "template1.gohtml", nil)
-	tpl.ExecuteTemplate(os.Stdout, "template2.gohtml", nil)
+	tpl.ExecuteTemplate(os.Stdout, "template1.gohtml", "blah")
+	tpl.ExecuteTemplate(os.Stdout, "template2.gohtml", "bloop")
 	tpl.ExecuteTemplate(os.Stdout, "template3.gohtml", nil)
+
+	//if we wanted to create a new file
+	newFile, err := os.Create("someFile")
+	if err != nil {
+		log.Fatalln("Error creating file.", err)
+	}
+	defer newFile.Close()
+
+	err2 := tpl.ExecuteTemplate(newFile, "template3.gohtml", "new file!")
+	if err2 != nil {
+		log.Fatalln("Error executing template.", err)
+	}
 }
